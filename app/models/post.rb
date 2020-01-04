bb4250bf9b2ea2a96892b21757df1aeb0fbd1dbd
yasuhiroh8160 <uriochan@icloud.com>
2020-01-04 16:43:48 +0900

    flash_message

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,39 @@ class Post < ApplicationRecord
   has_many :comments
 
   enum genre: { worry: 0, skill: 1, foreign: 2 }
+
+  validate :validate_content_attachment_byte_size
+  validate :validate_content_attachments_count
+
+  ONE_KILOBYTE = 1024
+  MAX_MEGA_BYTES = 0.5
+  MAX_CONTENT_ATTACHMENT_BYTE_SIZE = MAX_MEGA_BYTES * 1_000 * ONE_KILOBYTE
+  MAX_CONTENT_ATTACHMENTS_COUNT = 2
+
+  private
+
+  def validate_content_attachment_byte_size
+    content.body.attachables.grep(ActiveStorage::Blob).each do |attachable|
+      if attachable.byte_size > MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+        errors.add(
+          :base,
+          :content_attachment_byte_size_is_too_big,
+          max_content_attachment_mega_byte_size: MAX_MEGA_BYTES,
+          bytes: attachable.byte_size,
+          max_bytes: MAX_CONTENT_ATTACHMENT_BYTE_SIZE
+        )
+      end
+    end  
+  end
+
+  def validate_content_attachments_count
+    if content.body.attachables.grep(ActiveStorage::Blob).count > MAX_CONTENT_ATTACHMENTS_COUNT
+      errors.add(
+        :content,
+        :attachments_count_is_too_big,
+        max_content_attachments_count: MAX_CONTENT_ATTACHMENTS_COUNT
+      )
+    end
+  end
+
 end
